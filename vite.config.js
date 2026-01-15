9c76c381422bd01eb8901c9be6866364d85063a9
Qushl <vipioyt@mail.ru>
2026-01-15 21:06:12 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     port: 3000, // Порт по умолчанию
     host: true // Доступ с других устройств в сети
   },
+  base: './',
   build: {
     outDir: 'dist', // Папка для сборки
     emptyOutDir: true // Очищать папку при сборке
